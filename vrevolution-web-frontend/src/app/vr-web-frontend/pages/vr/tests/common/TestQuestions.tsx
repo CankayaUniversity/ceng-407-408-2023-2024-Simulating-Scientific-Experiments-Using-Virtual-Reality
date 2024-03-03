55f45d3e15378919df4591fc1e0c9c4c24942ce3
enesyagizyilmaz <enesy2lmz@icloud.com>
2024-03-03 14:54:57 +0300

    Development regarding the page where the titles of the tests are displayed

--- vrevolution-web-frontend/src/app/vr-web-frontend/pages/vr/tests/common/TestQuestions.tsx
+++ vrevolution-web-frontend/src/app/vr-web-frontend/pages/vr/tests/common/TestQuestions.tsx
@@ -1,22 +1,23 @@
-// TestQuestions.js
 import React from 'react';
 
 interface TestQuestionsProps {
     questions: {
         index: number;
         text: string;
-        options: { id: string; text: string }[];
+        options: { option_id: string; text: string }[];
     }[];
     selectedOptions: string[];
     handleOptionChange: (index: number, optionId: string) => void;
     calculateScore: () => void;
+
+    title: string
 }
 
-const TestHandler: React.FC<TestQuestionsProps> = ({ questions, selectedOptions, handleOptionChange, calculateScore }) => {
+const TestHandler: React.FC<TestQuestionsProps> = ({ questions, selectedOptions, handleOptionChange, calculateScore, title}) => {
     return (
         <div className="card shadow-sm">
             <div className="card-body">
-                <h1>Test 1 | Grade 5</h1>
+                <h1>{title}</h1>
             </div>
 
             <div className="card-footer">
@@ -29,15 +30,15 @@ const TestHandler: React.FC<TestQuestionsProps> = ({ questions, selectedOptions,
                                 </h2><br/>
                                 <ul style={{ listStyleType: 'none', padding: 0 }}>
                                     {question.options.map((option) => (
-                                        <li key={option.id}>
+                                        <li key={option.option_id}>
                                             <label style={{ display: 'flex', alignItems: 'center' }}>
                                                 <input
                                                     type="radio"
                                                     className={"form-check-input"}
                                                     name={`answer-${index}`}
-                                                    value={option.id}
-                                                    checked={option.id === selectedOptions[index]}
-                                                    onChange={() => handleOptionChange(index, option.id)}
+                                                    value={option.option_id}
+                                                    checked={option.option_id === selectedOptions[index]}
+                                                    onChange={() => handleOptionChange(index, option.option_id)}
                                                     style={{ marginRight: '8px' }}
                                                 />
                                                 {option.text}
